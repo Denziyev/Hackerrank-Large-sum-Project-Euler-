731648472c5649cd690e852cb6e8ddc5d7732fcf
İlkin <ilkinhd@code.edu.az>
2023-05-17 21:29:55 +0400

    solved

---
+++ Project_Euler _13-Large_sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        BigInteger sum=0;
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            sum += BigInteger.Parse(Console.ReadLine());
+        }
+        string result="1234567890";
+        StringBuilder sb = new StringBuilder(sum.ToString());
+        StringBuilder sb2 = new StringBuilder(result);
+        for (int i = 0;i < 10; i++)
+        {
+            sb2[i] = sb[i];
+        }
+
+        Console.WriteLine(sb2.ToString());
+    }
+}
